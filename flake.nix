0f061b2478a92d75ffa49c985ea17f205b15ed5b
Andrea Serra <andreaserradev-gbj@users.noreply.github.com>
2026-07-08 21:15:38 +0200

    Add standalone Home Manager flake skeleton

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Andrea's standalone Home Manager configuration";
+  
+  inputs = { 
+    
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-26.05";
+
+    home-manager = {
+	url = "github:nix-community/home-manager/release-26.05";
+	inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+  let
+    system = "aarch64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    homeConfigurations.andrea = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [ ./home.nix ];
+    };
+  };
+}
