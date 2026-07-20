e5f6a6b16438e946e40749e06ac0a13ebbade2f6
Andrea Serra <andreaserradev-gbj@users.noreply.github.com>
2026-07-20 20:04:03 +0200

    Fixed Emoji Font

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -3,10 +3,14 @@
   home.packages = [
     pkgs.nerd-fonts.jetbrains-mono
     pkgs.dejavu_fonts
+    pkgs.noto-fonts-color-emoji
   ];
 
   fonts.fontconfig = {
     enable = true;
-    defaultFonts.monospace = [ "JetBrainsMono Nerd Font" ];
+    defaultFonts = {
+      monospace = [ "JetBrainsMono Nerd Font" ];
+      emoji = [ "Noto Color Emoji" ];
+    };
   };
 }
