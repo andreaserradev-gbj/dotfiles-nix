50eb660700a50f8fc81f978021f11e1bf6dbbbf3
Andrea Serra <andreaserradev-gbj@users.noreply.github.com>
2026-09-23 07:37:45 +0200

    Bump omp to v18.2.11

--- flake.nix
+++ flake.nix
@@ -68,7 +68,7 @@
     # consumed by modules/home/omp.nix, gated on osConfig.local.dev.enable;
     # hplaptop (dev off) never evaluates it.
     omp = {
-      url = "github:can1357/oh-my-pi?ref=v18.2.10";
+      url = "github:can1357/oh-my-pi?ref=v18.2.11";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
       inputs.nixpkgs-darwin-x64.follows = "nixpkgs";
     };
